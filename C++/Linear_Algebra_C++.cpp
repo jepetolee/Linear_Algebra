44484d9dbf467c525d7c71eafbac1c4523e48066
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-01-26 21:47:47 +0900

    firstCommit

--- C++/Linear_Algebra_C++.cpp
+++ C++/Linear_Algebra_C++.cpp
@@ -1,1 +1,22 @@
 ﻿
+namespace linearAlgebra {
+	
+	union arraytype{
+		long double *array_float;
+		long long int *array_int;
+		char *array_char;
+		bool *array_boolean;
+	};
+	template <typename array >
+	class Matrix {
+		arraytype arrayType;
+
+
+		
+	};
+}
+
+int main() {
+	//linearAlgebra::Matrix::
+}
+
