9329ed83970f744f8dd9659e27b591aa3ea3b25c
jepetolee <55366212+jepetolee@users.noreply.github.com>
2022-02-23 16:28:54 +0900

    Update Matrix.h

--- C++/Matrix/Matrix.h
+++ C++/Matrix/Matrix.h
@@ -4,6 +4,8 @@ namespace linearAlgebra {
 	namespace Matrix {
 		template <typename Scalar>
 		class Matrix {
+		private:
+			vector<vector<Scalar>> Matrix;
 		public:
 			Matrix(unsigned int _rows,unsigned int _colums );
 
